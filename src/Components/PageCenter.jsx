26c6977eccee4c45c8b466e510c81b982ad80f3d
LouGel <l.gelinet@prontonmail.ch>
2022-09-16 17:45:56 +0200

    .mPageCenter settings

--- src/Components/PageCenter.jsx
+++ src/Components/PageCenter.jsx
@@ -24,7 +24,14 @@ export const PageCenter = ({ portable }) => {
   return (
     <div className={portable ? "mPageCenter" : "pageCenter"}>
       {portable ? (
-        <video className="vid" loop src={Video} autoPlay muted></video>
+        <video
+          className="vid"
+          loop
+          src={Video}
+          autoPlay
+          playsinline
+          muted
+        ></video>
       ) : (
         ""
       )}
